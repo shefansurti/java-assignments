d8415d1666a83627392f455a421dfe8887835807
shefansurti <41152888+shefansurti@users.noreply.github.com>
2018-07-23 00:32:51 +0530

    Delete even.java

--- even.java
+++
@@ -1,9 +0,0 @@
-class test{
-	public static void main(String[] args){
-		int num1=6;
-		if(num1>0)
-			if(num1<10)
-				if(num1%2==0)
-			System.out.println("num1 is between 0 & 10 and even");
-	}
-}
